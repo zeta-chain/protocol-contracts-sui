3b3f3bb1e49be67f4d1af30b5e8bb065ab0d939d
Denis Fadeev <denis@fadeev.org>
2025-07-28 17:34:11 +0300

    script

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -6,11 +6,18 @@ rm -rf docs
 mkdir -p docs
 rsync -a build/gateway/docs/gateway/ docs/
 
+if [[ "$(uname)" == "Darwin" ]]; then
+  # macOS (BSD sed)
+  sed_i() { sed -E -i '' "$@"; }
+else
+  # Linux (GNU sed)
+  sed_i() { sed -E -i "$@"; }
+fi
+
 find docs -name '*.md' -type f | while read -r file; do
-  sed -E -i.bak \
+  sed_i \
     -e 's/<a name="[^"]*"><\/a>//g' \
     -e 's/<a href="[^"]*"[^>]*>//g' \
     -e 's/<\/a>//g' \
     "$file"
-  rm -f "${file}.bak"
 done
